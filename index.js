198d36481817c512cebd54a30bf50b5ab4c698cb
fatma index <fgmal019@gmail.com>
2024-07-29 11:25:39 +0200

    new commit

--- index.js
+++ index.js
@@ -3,54 +3,64 @@
         {
             firstName: "john",
             lastName: "Doe",
-            age: 30,
-            phone: "(123) 456-7890"
+            age: '30',
+            phone: "123 456-7890"
         },
         {
             firstName: "emily",
             lastName: "Jones",
-            age: 35,
-            phone: "(345) 678-9012"
+            age: '35',
+            phone: "345 678-9012"
+        },
+        {
+            firstName: "emily",
+            lastName: "smith",
+            age: '20',
+            phone: "345 567-9012"
         },
         {
             firstName: "jane",
             lastName: "Smith",
-            age: 25,
-            phone: "(234) 567-8901"
+            age: '25',
+            phone: "234 567-8901"
         },
         {
             firstName: "sarah",
             lastName: "Davis",
-            age: 28,
-            phone: "(567) 890-1234"
+            age: '28',
+            phone: "567 890-1234"
         },
         {
             firstName: "david",
             lastName: "Wilson",
-            age: 32,
-            phone: "(678) 901-2345"
+            age: '32',
+            phone: "678 901-2345"
         }
     ];
+    // ----------------------------------------search function---------------------------------------------//
     function search(val) {
         var table = document.querySelector(".table");
-        table.innerHTML = "";
         var found = false;
         arr.forEach(function (obj) {
-            if (val === obj.firstName.toLowerCase()) {
+            if (val === obj.firstName.toLowerCase() || val === obj.age || val === obj.phone) {
                 table.innerHTML += "<tr>\n                    <td>".concat(obj.firstName, "</td>\n                    <td>").concat(obj.lastName, "</td>\n                    <td>").concat(obj.age, "</td>\n                    <td>").concat(obj.phone, "</td>\n                </tr>");
                 found = true;
+                // searchInput.value = ""
             }
         });
         if (!found) {
             table.innerHTML += "<tr><td style=\"text-align:center\" colspan=\"4\">No results found</td></tr>";
         }
     }
-    function autocomplete(val) {
-        var autocompleteList = document.getElementById("autocomplete-list");
+    // --------------------------------------firstname auto-complete function---------------------------------//
+    var autocompleteList = document.getElementById("autocomplete-list");
+    var autocompleteList2 = document.getElementById("autocomplete-list2");
+    var autocompleteList3 = document.getElementById("autocomplete-list3");
+    function autocomplete1(val) {
         autocompleteList.innerHTML = "";
         if (!val)
             return;
-        var filtered = arr.filter(function (person) { return person.firstName.toLowerCase().startsWith(val.toLowerCase()); });
+        var filtered = arr.filter(function (person) { return person.firstName.toLowerCase().includes(val.toLowerCase()); });
         filtered.forEach(function (person) {
             var item = document.createElement("div");
             item.innerHTML = "".concat(person.firstName);
@@ -62,16 +72,65 @@
             });
             autocompleteList.appendChild(item);
         });
-        // autocompleteList.style.display = filtered.length ? 'block' : 'none'; 
     }
+    // ---------------------------------------age auto-complete function-------------------------------------------//
+    function autocomplete2(age) {
+        autocompleteList2.innerHTML = "";
+        if (!age)
+            return;
+        var filtered = arr.filter(function (person) { return person.age.includes(age.trim()); });
+        filtered.forEach(function (person) {
+            var item = document.createElement("div");
+            item.innerHTML = "".concat(person.age);
+            item.addEventListener("click", function () {
+                var searchInput2 = document.getElementById("inputField2");
+                searchInput2.value = person.age;
+                search(person.age);
+                autocompleteList2.innerHTML = "";
+            });
+            autocompleteList2.appendChild(item);
+        });
+    }
+    // ----------------------------------phone-number auto-complete function--------------------------------------//
+    function autocomplete3(phoneNum) {
+        autocompleteList3.innerHTML = "";
+        if (!phoneNum)
+            return;
+        var filtered = arr.filter(function (person) { return person.phone.includes(phoneNum.trim()); });
+        filtered.forEach(function (person) {
+            var item = document.createElement("div");
+            item.innerHTML = "".concat(person.phone);
+            item.addEventListener("click", function () {
+                var searchInput3 = document.getElementById("inputField3");
+                searchInput3.value = person.phone;
+                search(person.phone);
+                autocompleteList3.innerHTML = "";
+            });
+            autocompleteList3.appendChild(item);
+        });
+    }
+    //--------------------------add event listener to the search inputs-------------------------------//
     var searchInput = document.getElementById("inputField");
     searchInput.addEventListener("input", function () {
+        autocompleteList2.innerHTML = "";
+        autocompleteList3.innerHTML = "";
         var val = searchInput.value.trim().toLowerCase();
-        autocomplete(val);
+        autocomplete1(val);
     });
-    var searchBtn = document.getElementById("search-btn");
-    searchBtn.addEventListener("click", function () {
-        var val = searchInput.value.trim().toLowerCase();
-        search(val);
+    // --------------------------------------------------------------------------------------------//
+    var searchInput2 = document.getElementById("inputField2");
+    searchInput2.addEventListener("input", function () {
+        autocompleteList.innerHTML = "";
+        autocompleteList3.innerHTML = "";
+        var num = searchInput2.value;
+        autocomplete2(num);
+    });
+    // --------------------------------------------------------------------------------------------//
+    var searchInput3 = document.getElementById("inputField3");
+    searchInput3.addEventListener("input", function () {
+        autocompleteList2.innerHTML = "";
+        autocompleteList.innerHTML = "";
+        var phoneNum = searchInput3.value;
+        autocomplete3(phoneNum);
     });
 })();
--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 (() => {
+
+    // -----------------------------------USER'S DATA----------------------------------------
     type Person = {
         firstName: string;
         lastName: string;
-        age: number;
+        age: string;
         phone: string;
     };
 
@@ -10,44 +12,49 @@
         {
             firstName: "john",
             lastName: "Doe",
-            age: 30,
-            phone: "(123) 456-7890"
+            age: '30',
+            phone: "123 456-7890"
         },
-        
+
         {
             firstName: "emily",
             lastName: "Jones",
-            age: 35,
-            phone: "(345) 678-9012"
+            age: '35',
+            phone: "345 678-9012"
+        },
+        {
+            firstName: "emily",
+            lastName: "smith",
+            age: '20',
+            phone: "345 567-9012"
         },
         {
             firstName: "jane",
             lastName: "Smith",
-            age: 25,
-            phone: "(234) 567-8901"
+            age: '25',
+            phone: "234 567-8901"
         },
         {
             firstName: "sarah",
             lastName: "Davis",
-            age: 28,
-            phone: "(567) 890-1234"
+            age: '28',
+            phone: "567 890-1234"
         },
         {
             firstName: "david",
             lastName: "Wilson",
-            age: 32,
-            phone: "(678) 901-2345"
+            age: '32',
+            phone: "678 901-2345"
         }
     ];
-
+    // ----------------------------------------search function---------------------------------------------//
     function search(val: string) {
         const table = document.querySelector(".table") as HTMLElement;
-        table.innerHTML = "";
 
         let found = false;
 
         arr.forEach(obj => {
-            if (val === obj.firstName.toLowerCase()) {
+            if (val === obj.firstName.toLowerCase() || val === obj.age || val === obj.phone) {
                 table.innerHTML += `<tr>
                     <td>${obj.firstName}</td>
                     <td>${obj.lastName}</td>
@@ -55,6 +62,7 @@
                     <td>${obj.phone}</td>
                 </tr>`;
                 found = true;
+
             }
         });
 
@@ -63,13 +71,19 @@
         }
     }
 
-    function autocomplete(val: string) {
-        const autocompleteList = document.getElementById("autocomplete-list") as HTMLElement;
+
+    // --------------------------------------firstname auto-complete function---------------------------------//
+    const autocompleteList = document.getElementById("autocomplete-list") as HTMLElement;
+
+    const autocompleteList2 = document.getElementById("autocomplete-list2") as HTMLElement;
+    const autocompleteList3 = document.getElementById("autocomplete-list3") as HTMLElement;
+
+    function autocomplete1(val: string) {
         autocompleteList.innerHTML = "";
 
         if (!val) return;
 
-        const filtered = arr.filter(person => person.firstName.toLowerCase().startsWith(val.toLowerCase()));
+        const filtered = arr.filter(person => person.firstName.toLowerCase().includes(val.toLowerCase()));
 
         filtered.forEach(person => {
             const item = document.createElement("div");
@@ -78,24 +92,85 @@
                 const searchInput = document.getElementById("inputField") as HTMLInputElement;
                 searchInput.value = person.firstName;
                 search(person.firstName.toLowerCase());
-                autocompleteList.innerHTML = ""; 
+                autocompleteList.innerHTML = "";
             });
             autocompleteList.appendChild(item);
         });
 
     }
+    // ---------------------------------------age auto-complete function-------------------------------------------//
+
+    function autocomplete2(age: string) {
+        autocompleteList2.innerHTML = "";
+
+        if (!age) return;
+
+        const filtered = arr.filter(person => person.age.includes(age.trim()));
+
+        filtered.forEach(person => {
+            const item = document.createElement("div");
+            item.innerHTML = `${person.age}`;
+            item.addEventListener("click", () => {
+                const searchInput2 = document.getElementById("inputField2") as HTMLInputElement;
+                searchInput2.value = person.age;
+                search(person.age);
+                autocompleteList2.innerHTML = "";
+            });
+            autocompleteList2.appendChild(item);
+        });
+
+    }
+    // ----------------------------------phone-number auto-complete function--------------------------------------//
+
+    function autocomplete3(phoneNum: string) {
+        autocompleteList3.innerHTML = "";
+
+        if (!phoneNum) return;
+
+        const filtered = arr.filter(person => person.phone.includes(phoneNum.trim()));
+
+        filtered.forEach(person => {
+            const item = document.createElement("div");
+            item.innerHTML = `${person.phone}`;
+            item.addEventListener("click", () => {
+                const searchInput3 = document.getElementById("inputField3") as HTMLInputElement;
+                searchInput3.value = person.phone;
+                search(person.phone);
+                autocompleteList3.innerHTML = "";
+            });
+            autocompleteList3.appendChild(item);
+        });
+
+    }
+
+    //--------------------------add event listener to the search inputs-------------------------------//
     const searchInput = document.getElementById("inputField") as HTMLInputElement;
 
     searchInput.addEventListener("input", () => {
+        autocompleteList2.innerHTML = "";
+        autocompleteList3.innerHTML = "";
         const val = searchInput.value.trim().toLowerCase();
-        autocomplete(val);
+        autocomplete1(val);
+    });
+    // --------------------------------------------------------------------------------------------//
+    const searchInput2 = document.getElementById("inputField2") as HTMLInputElement;
+
+    searchInput2.addEventListener("input", () => {
+        autocompleteList.innerHTML = "";
+        autocompleteList3.innerHTML = "";
+        const num = searchInput2.value;
+        autocomplete2(num);
+    });
+    // --------------------------------------------------------------------------------------------//
+
+    const searchInput3 = document.getElementById("inputField3") as HTMLInputElement;
+
+    searchInput3.addEventListener("input", () => {
+        autocompleteList2.innerHTML = "";
+        autocompleteList.innerHTML = "";
+        const phoneNum = searchInput3.value;
+        autocomplete3(phoneNum);
     });
 
-    const searchBtn = document.getElementById("search-btn") as HTMLButtonElement;
 
-    searchBtn.addEventListener("click", () => {
-        
-        const val = searchInput.value.trim().toLowerCase();
-        search(val);
-    });
 })();
